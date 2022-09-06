79455e503f870f33233830e6f61bb2c10acebab5
Jur Van toledo <jvan-tol@f1r2s19.codam.nl>
2022-09-06 15:15:16 +0200

    added libft and makefile

---
+++ Makefile
@@ -0,0 +1,32 @@
+NAME = minishell
+CC = gcc
+FLAGS = -Wall -Werror -Wextra
+INCLUDE = include
+LIBFT_DIR = $(INCLUDE)/libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRC = src/main.c
+
+OBJ = $(SRC:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ) $(LIBFT)
+	$(CC) $(FLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(FLAGS) -c $< -o $@
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+clean:
+	$(MAKE) fclean -C $(LIBFT_DIR)
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- include/minishell.h
+++ include/minishell.h
@@ -6,13 +6,14 @@
 /*   By: jvan-tol <jvan-tol@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/06 14:39:42 by jvan-tol      #+#    #+#                 */
-/*   Updated: 2022/09/06 14:40:16 by jvan-tol      ########   odam.nl         */
+/*   Updated: 2022/09/06 15:08:42 by jvan-tol      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 
 # include <stdio.h>
+# include "./libft/libft.h"
 
 # define MINISHELL_H
 
