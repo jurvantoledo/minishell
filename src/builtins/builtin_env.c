0e2bc89ddc8ef0dfc8e87391cfd0e7a1a9689797
Jur Van toledo <jvan-tol@f0r2s18.codam.nl>
2022-10-24 14:32:20 +0200

    fixed env

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: jvan-tol <jvan-tol@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/21 15:44:12 by jvan-tol      #+#    #+#                 */
-/*   Updated: 2022/10/21 18:25:52 by jvan-tol      ########   odam.nl         */
+/*   Updated: 2022/10/24 14:31:27 by jvan-tol      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int	builtin_env(void)
 {
+	char	*str;
 	t_env	*env;
 
 	env = g_shell.env;
-	if (!env)
+	if (!env || !env->next)
 		return (0);
-	while (env)
+	while (env->next)
 	{
 		ft_putstr_fd(env->key, 1);
 		ft_putchar_fd('=', 1);
-		ft_putstr_fd(env->value, 1);
-		ft_putchar_fd('\n', 1);
+		ft_putendl_fd(env->value, 1);
 		env = env->next;
 	}
 	return (1);
